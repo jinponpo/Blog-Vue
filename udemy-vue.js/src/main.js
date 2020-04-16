68e9c5448995ccec64fe42669d37fb49ef937516
jinponpo <jk.kunia@gmail.com>
2020-04-16 16:36:47 +0900

    1st

---
+++ udemy-vue.js/src/main.js
@@ -0,0 +1,19 @@
+import Vue from 'vue'
+import App from './App.vue'
+import axios from 'axios'
+import router from './router';
+import store from './store';
+
+Vue.config.productionTip = false
+
+axios.defaults.baseURL =
+  "https://firestore.googleapis.com/v1/projects/vuejs-http-a75c1/databases/(default)/documents"
+
+store.dispatch('autoLogin')
+.then(() => {
+  new Vue({
+    router,
+    store,
+    render: h => h(App),
+  }).$mount('#app')
+});
